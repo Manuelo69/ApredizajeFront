479e0981e5ca60ebf4a05848b4cdb0c4e2f8d2ce
manuelo69 <ichigo2418@gmail.com>
2024-04-25 14:36:31 +0200

    configuracion de variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-gray": "#f7f7f7",
+        "my-gray": "#e2e2e2",
+        "dark-gray": "#888888",
+        "darker-gray": "#666666",
+        "main-green": "#5ee85b",
+      },
+      fontSize: {
+        "text-xs": "12px",
+        "text-sm": " 14px",
+        "text-base": "16px",
+        "text-md": "18px",
+        "text-xl": "40px",
+        "text-2xl": "64px",
+      },
+    },
   },
   plugins: [],
 };
