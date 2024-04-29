094423a44d920ee27ffa4411f682897746484cb3
manuelo69 <ichigo2418@gmail.com>
2024-04-29 14:10:16 +0200

    Creacion de componente funnal

---
+++ src/components/Funnal/Funnal.jsx
@@ -0,0 +1,20 @@
+const Funnal = () => {
+  return (
+    <div className="fun">
+      <button>
+        <img
+          src="https://cdn.hugeicons.com/icons/crowdfunding-stroke-rounded.svg"
+          alt="crowdfunding"
+        />
+      </button>
+      <p>funnal</p>
+      <button className="selectorRow">
+        <img
+          src="https://cdn.hugeicons.com/icons/arrow-down-01-stroke-rounded.svg"
+          alt="arrow-down-01"
+        />
+      </button>
+    </div>
+  );
+};
+export default Funnal;
