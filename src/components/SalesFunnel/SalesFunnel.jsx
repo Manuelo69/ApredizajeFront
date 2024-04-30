57c9a8bd681f712b90061fa88a2214c5301a229d
manuelo69 <ichigo2418@gmail.com>
2024-04-30 10:30:25 +0200

    Reajustes del contenido

--- src/components/SalesFunnel/SalesFunnel.jsx
+++ src/components/SalesFunnel/SalesFunnel.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/prop-types */
+import Views from "../Ui/Views";
 const SalesFunnel = ({ titulo, subtitulo }) => {
   return (
     <div className="flex flex-col  bg-light-gray  rounded-3xl p-3 w-px-900 h-96">
-      <div className="flex flex-row justify-between ">
+      <div className="flex flex-row justify-between items-center h-40">
         <div className="flex flex-col items-start">
           <p className="text-text-md">{titulo}</p>
           <p className="text-text-xs">{subtitulo}</p>
         </div>
-        <div className="flex flex-row gap-1 h-10">
+        <div className="flex flex-row gap-1 h-10 items-center">
           <button className="w-20 p-4 bg-white">Monthly</button>
           <button className="p-4 text-text-sm bg-white">
             <img
@@ -17,28 +18,15 @@ const SalesFunnel = ({ titulo, subtitulo }) => {
           </button>
         </div>
       </div>
-      <div className="flex flex-row justify-between w-96">
-        <div className="barra b1">
-          {}
-          <div className="cant">24K</div>
-        </div>
-        <div className="barra b2">
-          <div className="cant">24K</div>
-        </div>
-        <div className="barra b3">
-          <div className="cant">24K</div>
-        </div>
-        <div className="barra b4">
-          <div className="cant">24K</div>
-        </div>
-        <div className="barra b5">
-          <div className="cant">24K</div>
-        </div>
-        <div className="barra b6">
-          <div className="cant">24K</div>
-        </div>
+      <div className="flex flex-row justify-center w-px-800 gap-3 h-80">
+        <Views />
+        <Views />
+        <Views />
+        <Views />
+        <Views />
+        <Views />
       </div>
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row justify-around w-px-800">
         <p>Mon</p>
         <p>Tue</p>
         <p>Wed</p>
---
+++ src/components/Ui/Views.jsx
@@ -0,0 +1,10 @@
+const views = () => {
+  return (
+    <div className="flex flex-col h-60 w-96 bg-black">
+      <div className="cant">24K</div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default views;
