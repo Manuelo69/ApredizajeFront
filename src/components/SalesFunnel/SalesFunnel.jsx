a22098fb66669b9a38847239060a7a296852a4e0
manuelo69 <ichigo2418@gmail.com>
2024-04-30 10:12:15 +0200

    colocacion del componente SalesFunnel

--- src/components/SalesFunnel/SalesFunnel.jsx
+++ src/components/SalesFunnel/SalesFunnel.jsx
@@ -2,14 +2,14 @@
 const SalesFunnel = ({ titulo, subtitulo }) => {
   return (
     <div className="flex flex-col  bg-light-gray  rounded-3xl p-3 w-px-900 h-96">
-      <div className="flex flex-row justify-between">
-        <div className="textoIzquierdo">
-          <p>Sales Funnel</p>
-          <p>Total view per month</p>
+      <div className="flex flex-row justify-between ">
+        <div className="flex flex-col items-start">
+          <p className="text-text-md">Sales Funnel</p>
+          <p className="text-text-xs">Total view per month</p>
         </div>
         <div className="flex flex-row gap-1 h-10">
-          <button className="w-20 p-2">Monthly</button>
-          <button className="p-2">
+          <button className="w-20 p-4 bg-white">Monthly</button>
+          <button className="p-4 text-text-sm bg-white">
             <img
               src="https://cdn.hugeicons.com/icons/dashboard-square-remove-stroke-rounded.svg"
               alt="dashboard-square-remove"
@@ -17,16 +17,17 @@ const SalesFunnel = ({ titulo, subtitulo }) => {
           </button>
         </div>
       </div>
-      <div className="sales">
-        <div className="cant">24K</div>
-        <div className="barra b1"></div>
+      <div className="flex flex-row justify-between w-96">
+        <div className="barra b1">
+          <div className="cant">24K</div>
+        </div>
         <div className="barra b2"></div>
         <div className="barra b3"></div>
         <div className="barra b4"></div>
         <div className="barra b5"></div>
         <div className="barra b6"></div>
       </div>
-      <div className="days">
+      <div className="flex flex-row justify-between">
         <p>Mon</p>
         <p>Tue</p>
         <p>Wed</p>
